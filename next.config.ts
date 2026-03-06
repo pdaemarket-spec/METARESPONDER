25b70f095dcf1a2cd870aedc7cd6fff6f5122785
Jose Antonio Blanco <pda.emarket@gmail.com>
2026-03-05 22:47:49 -0400

    Fix webpack alias

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // Configuración de Next.js
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, './src'),
+    }
+    return config
+  },
 }
 
 export default nextConfig
